b005c5cad0f24862787d2adef55e516406c5c23b
beenUpFor3Days <1289438@learn.vsb.bc.ca>
2025-10-02 21:38:15 +0000

    compare

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -1,4 +1,4 @@
-"""
+""
 Create a program that uses counting and comparison operators (< > <= >=).
 You must use the class' datafile, 'responses.csv' and analyze it
     to make at least 2 interesting observations.
@@ -6,3 +6,5 @@ You must use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
+
+
