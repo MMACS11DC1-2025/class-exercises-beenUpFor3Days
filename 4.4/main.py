2fb19d5c27f8b58b87ce36a27448921fc4d5bae8
beenUpFor3Days <1289438@learn.vsb.bc.ca>
2025-11-07 07:28:46 +0000

    6767676767676767676767676417647146174617461741414141414141414141414141676767676767

--- 4.4/main.py
+++ 4.4/main.py
@@ -1,4 +1,5 @@
 import turtle
+
 SETTINGS = {
     "branchLength": 67.41,
     "levels": 3,
@@ -6,40 +7,46 @@ SETTINGS = {
     "leafColour": "green"
 }
 
-turtle = turtle.Turtle()
+pen = turtle.Turtle()
+pen.width(4.1)
 
-def drawTree(level, branchLength):
+def drawTree(t, level, branchLength):
   if level > 0:
-    turtle.pencolor(colour1)
-    turtle.forward(branchLength)
-    turtle.left(21)
-    drawTree(level-1, branchLength/1.41)
-    
-    turtle.right(21)
-    drawTree(level-1, branchLength/1.41)
-    
-    turtle.right(21)
-    drawTree(level-1, branchLength/1.41)
-    
+    t.pencolor(colour1)
+    t.forward(branchLength)
+
+    t.left(21)
+    drawTree(t, level - 1, branchLength / 1.67)
+    t.right(21)
+
+    t.right(21)
+    drawTree(t, level - 1, branchLength / 1.67)
+    t.left(21)
+
+    t.backward(branchLength)
+
   else:
-    turtle.pencolor(colour2)
-    turtle.color("")
-    turtle.stamp()
-    turtle.color("brown")
-turtle.speed(0)
-turtle.penup()
-turtle.goto(0, -180)
-turtle.left(90)
-turtle.pendown()
+    t.pencolor(colour2)
+    t.dot(6)
+    t.pencolor(colour1)
+    
+pen.speed(0)
+pen.penup()
+pen.goto(0, -180)
+pen.left(90)
+pen.pendown()
 
-levels = input("How many levels do you want me to draw. (defualt is 3) ") or SETTINGS["levels"]
-branchLength = input('How long do you want each branch to be. (defualt is 67.41)') or SETTINGS["branchLength"]
-colour1 = input("What colour do you want your brach to be. (defualt is brown) ") or SETTINGS["branchColour"]
-colour2 = input("what do you want the branch colour to be. (defualt is green)") or SETTINGS["leafColour"]
-if levels <1:
-  levels =1
+levels = int(input("How many levels do you want me to draw. (default is 3) ") or SETTINGS["levels"])
+branchLength = float(input("How long do you want each branch to be. (default is 67.41) ") or SETTINGS["branchLength"])
 
-if branchLength <10:
+if levels < 1:
+  levels = 1
+if branchLength < 10:
   branchLength = 10
 
-drawTree(int(levels), 120)
+colour1 = input("What colour do you want your branch to be. (default is brown) ") or SETTINGS["branchColour"]
+colour2 = input("What colour do you want the leaves to be. (default is green) ") or SETTINGS["leafColour"]
+
+drawTree(pen, levels, branchLength)
+pen.hideturtle()
+turtle.done()
