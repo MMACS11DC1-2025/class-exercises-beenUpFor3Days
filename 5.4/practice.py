e236628bfa91f01ccbac3f2dfba16ee178e18a85
beenUpFor3Days <1289438@learn.vsb.bc.ca>
2025-11-19 21:31:00 +0000

    jelly beans

---
+++ 5.4/practice.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+def is_yellow(r,g,b):
+    if r > 150 and g > 150 and b < 150:
+        return True
+    return False
+
+image = Image.open("5.4/jelly_beans.jpg").load()
+image_output = Image.open("5.4/jelly_beans.jpg")
+width = image_output.width
+height = image_output.height
+
+for i in range(width):
+    for j in range(height):
+        im_r = image[i, j][0]
+        im_g = image[i, j][1]
+        im_b = image[i, j][2]
+
+        if is_yellow(im_r, im_g, im_b):
+            image_output.putpixel((i, j), (255, 255, 255))
+
+image_output.save("newBeans.png", "png")
